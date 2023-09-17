8b4290cc7de4e998a02beb85bcad74ca3f68b24c
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-17 18:54:20 -0300

    feat: create SanitizeMessage function

---
+++ messages/message.go
@@ -0,0 +1,14 @@
+package messages
+
+import "github.com/allancapistrano/tangle-client-go/utils"
+
+type Message struct {
+	Index   string `json:"index"`
+	Content string `json:"content"`
+}
+
+// Sanitizes a message.
+func SanitizeMessage(message *Message) {
+	message.Content = utils.SanitizeString(message.Content)
+	message.Index = utils.SanitizeString(message.Index)
+}
