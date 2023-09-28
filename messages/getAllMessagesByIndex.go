a644e98ce62f7125a20ac2eb46b590b9fff48883
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-27 21:11:41 -0300

    chore: moving format message functions

--- messages/getAllMessagesByIndex.go
+++ messages/getAllMessagesByIndex.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
-	"strings"
 
-	"github.com/allancapistrano/tangle-client-go/utils"
 	iotago "github.com/iotaledger/iota.go/v2"
 )
 
@@ -61,53 +59,4 @@ func GetAllMessagesByIndex(nodeUrl string, index string) ([]Message, error) {
 	}
 
 	return messages, nil
-}
-
-// Formats the message payload into a custom message type.
-func formatMessagePayload(message iotago.Message, messageIndex string) (Message, error) {
-	payloadInString, err := utils.SerializeMessagePayload(message.Payload, true)
-	if err != nil {
-		return Message{}, err
-	}
-
-	index := ""
-	content := ""
-
-	if strings.Contains(payloadInString, "/") {
-		payloadTemp := strings.Split(payloadInString, "/")
-
-		index = payloadTemp[0]
-		content = payloadTemp[1]
-	} else if strings.Contains(payloadInString, "\v") {
-		payloadTemp := strings.Split(payloadInString, "\v")
-
-		if len(payloadTemp) == 2 {
-			index = payloadTemp[0]
-			content = payloadTemp[1]
-		} else if len(payloadTemp) == 3 {
-			index = payloadTemp[1]
-			content = payloadTemp[2]
-		} else {
-			return Message{}, errors.New("unexpected array length")
-		}
-	} else if strings.Contains(payloadInString, "\t") {
-		payloadTemp := strings.Split(payloadInString, "\t")
-
-		index = payloadTemp[0]
-		content = payloadTemp[1]
-	} else if strings.Contains(payloadInString, messageIndex) {
-		payloadTemp := strings.Split(payloadInString, messageIndex)
-
-		index = messageIndex
-		content = payloadTemp[1]
-	} else {
-		return Message{}, errors.New("malformed payload")
-	}
-
-	formattedMessage := Message{
-		Index:   strings.Trim(index, "\f"),
-		Content: strings.Trim(content, "\f"),
-	}
-
-	return formattedMessage, nil
 }
--- messages/getMessageByMessageId.go
+++ messages/getMessageByMessageId.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
-	"strings"
 
-	"github.com/allancapistrano/tangle-client-go/utils"
 	iotago "github.com/iotaledger/iota.go/v2"
 )
 
@@ -54,48 +52,4 @@ func GetMessageFormattedByMessageID(nodeUrl string, messageIdHex string) (Messag
 	}
 
 	return message, nil
-}
-
-// Formats the message payload into a custom message type.
-func formatMessagePayloadWithoutIndex(message *iotago.Message) (Message, error) {
-	payloadInString, err := utils.SerializeMessagePayload(message.Payload, true)
-	if err != nil {
-		return Message{}, err
-	}
-
-	index := ""
-	content := ""
-
-	if strings.Contains(payloadInString, "/") {
-		payloadTemp := strings.Split(payloadInString, "/")
-
-		index = payloadTemp[0]
-		content = payloadTemp[1]
-	} else if strings.Contains(payloadInString, "\v") {
-		payloadTemp := strings.Split(payloadInString, "\v")
-
-		if len(payloadTemp) == 2 {
-			index = payloadTemp[0]
-			content = payloadTemp[1]
-		} else if len(payloadTemp) == 3 {
-			index = payloadTemp[1]
-			content = payloadTemp[2]
-		} else {
-			return Message{}, errors.New("unexpected array length")
-		}
-	} else if strings.Contains(payloadInString, "\t") {
-		payloadTemp := strings.Split(payloadInString, "\t")
-
-		index = payloadTemp[0]
-		content = payloadTemp[1]
-	} else {
-		return Message{}, errors.New("malformed payload")
-	}
-
-	formattedMessage := Message{
-		Index:   strings.Trim(index, "\f"),
-		Content: strings.Trim(content, "\f"),
-	}
-
-	return formattedMessage, nil
 }
